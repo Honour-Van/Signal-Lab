3335bb724a38472138cb96fb5186eb40519b8da9
fhn <fhn037@126.com>
2021-01-04 00:36:56 +0800

    :sparkles:add simplest filtering

---
+++ simplest.m
@@ -0,0 +1,13 @@
+clear; close all; clc;
+load('data_basic_student_new.mat')
+load('basicsimplest.mat')
+rt = r_training(1, :);
+st = [s_training(1, :), zeros(1, 3)];
+sr = filter(tfb, tfa, rt);
+delta = st - sr;
+N = 8195; K = 0;
+Perr = sum(delta.^2) / N;
+U = K + log10(Perr);
+fprintf('U = %f\n', U);
+rt1 = rt;
+tdplot
